1543605aede4ad804a8340d14d36670688abce31
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-11 21:01:47 -0300

    FEAT : (arrays) create and configure data structure

--- src/model/allfavorites.ts
+++ src/model/allfavorites.ts
@@ -1,24 +1,45 @@
-export const AllGithubers = [
-  {pfp : "https://avatars.githubusercontent.com/u/143217996?v=4", name : "Dinho silva", stack : "Frontend", follow : 12, repo : 32, completetime : 3},
-  {pfp : "https://avatars.githubusercontent.com/u/143217996?v=4", name : "Dinho silva", stack : "Frontend", follow : 12, repo : 32, completetime : 3},
-  {pfp : "https://avatars.githubusercontent.com/u/143217996?v=4", name : "Dinho silva", stack : "Frontend", follow : 12, repo : 32, completetime : 3},
-  {pfp : "https://avatars.githubusercontent.com/u/143217996?v=4", name : "Dinho silva", stack : "Frontend", follow : 12, repo : 32, completetime : 3},
-  {pfp : "https://avatars.githubusercontent.com/u/143217996?v=4", name : "Dinho silva", stack : "Frontend", follow : 12, repo : 32, completetime : 3},
-  {pfp : "https://avatars.githubusercontent.com/u/143217996?v=4", name : "Dinho silva", stack : "Frontend", follow : 12, repo : 32, completetime : 3},
+export interface AllGithubersTypes {
+  pfp?: string;
+  name: string;
+  stack: string;
+  follow: number;
+  repo: number;
+  createTime: number;
+
+}
+export const AllGithubers: AllGithubersTypes[] = [
+  {
+    pfp: "https://avatars.githubusercontent.com/u/143217996?v=4",
+    name: "Dinho silva",
+    stack: "Frontend",
+    follow: 12,
+    repo: 32,
+    createTime: 3
+  },
+
 ]
 
-export const recentGithubers = [
-  {pfp : "https://avatars.githubusercontent.com/u/143217996?v=4", name : "Dinho silva", stack : "Frontend", follow : 12, repo : 32, completetime : 3},
-  {pfp : "https://avatars.githubusercontent.com/u/143217996?v=4", name : "Dinho silva", stack : "Frontend", follow : 12, repo : 32, completetime : 3},
-  {pfp : "https://avatars.githubusercontent.com/u/143217996?v=4", name : "Dinho silva", stack : "Frontend", follow : 12, repo : 32, completetime : 3},
-  {pfp : "https://avatars.githubusercontent.com/u/143217996?v=4", name : "Dinho silva", stack : "Frontend", follow : 12, repo : 32, completetime : 3},
+
+export const recentGithubers: AllGithubersTypes[] = [
+  {
+    pfp: "https://avatars.githubusercontent.com/u/143217996?v=4",
+    name: "Dinho silva",
+    stack: "Frontend",
+    follow: 12,
+    repo: 32,
+    createTime: 3
+  },
 ]
 
-export const stacksList = [
+export interface stacksListType {
+  name: string;
+}
 
-  {name : "Frontend"},
-  {name : "Backend"},
-  {name : "Devops"},
-  {name : "Mobile"},
+export const stacksList: stacksListType[] = [
+
+  { name: "Frontend" },
+  { name: "Backend" },
+  { name: "Devops" },
+  { name: "Mobile" },
 
 ]
