556e43d39d57c06ea8548c07e55041de7506e7f0
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-12 13:42:39 -0300

    FEAT : added favorites list rendering

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { useState } from 'react'
 import './App.css'
 import { UserFound } from './components/userfound'
 import { formaterForSearch } from './utils/textformatter'
+import useUserGlobalData from './store.ts/favoriteusers'
 
 function App() {
 
   const [inputTerm, setinputTerm] = useState("")
   const [githuber, setgithuber] = useState('')
+  const {data, removeData} = useUserGlobalData()
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
@@ -18,6 +20,7 @@ function App() {
   const searchGithubers = () => {
     setgithuber(inputTerm)
   }
+
 
   return (
 
@@ -46,10 +49,37 @@ function App() {
      }
       <section>
         <h2>Recentes</h2>
+
+<ul>
+{ data ? data.map(({name, bio, avatar_url, public_repos, followers,  totalStart}, index) =>(
+         
+          <li key={index}>
+            <figure>
+              <img src={avatar_url} alt={`${name}`} style={{width : "10%"}} />
+            </figure>
+            <header>
+              <h2> {name}</h2>
+              <p>{bio}</p>
+
+              <ul>
+                <li>{public_repos}</li>
+                <li>{followers}</li>
+                <li>{totalStart}</li>
+
+
+              </ul>
+            </header>
+           
+           <button type='button' onClick={removeData(name)}>Remover</button>
+          
+          </li>
+        )) : "Nehum Favorito Ainda" }
+</ul>
       </section>
 
       <section>
         <h2>Favoritos</h2>
+      
 
         <ul>
           <li>Frontend </li>
