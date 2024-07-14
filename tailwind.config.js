04cf5271b1f83e29d959f8e49ea1ca0ff3724085
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-14 07:21:52 -0300

    STYLE : add personal style

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        'purple-light': '#EBE5FF',
+        'purple-dark': '#2C0076',
+        'yellow-icon': '#FCC313'
+      },
+      fontFamily : {
+        "Montserrat": ["Montserrat", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
