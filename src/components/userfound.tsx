558a9e1207715409d8fa6b01adf643fa26162199
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-14 07:17:03 -0300

    FEAT : create a new style estructure

--- src/components/userfound.tsx
+++ src/components/userfound.tsx
@@ -1,8 +1,9 @@
 import React from "react"
 import { useGitHubUsers } from "../hooks/fetchhooks"
 import { FavoriteProps, useProfileProps } from "../@types/githubusers"
-import { Star } from "lucide-react"
 import useUserGlobalData from "../store.ts/favoriteusers"
+import { Archive, Star, User2 } from "lucide-react";
+
 
 export const UserFound: React.FC<useProfileProps> = ({ username }) => {
   const { user, isLoading, isError, totalStart } = useGitHubUsers(username)
@@ -26,27 +27,52 @@ const alldata : FavoriteProps = {name, bio, avatar_url , public_repos, followers
 upDateData(alldata)
     
   }
-  return(
-<>
-<section>
-<figure>
-<img src={user.avatar_url} alt={`${user.name}`} style={{width : "10%"}}/>
+  return (
+    <>
+      <section className="flex justify-between items-center bg-purple-light w-[90%] ml-auto mr-auto py-8 px-4 rounded-lg font-Montserrat">
+        <header className="flex border flex-col">
+          <img
+            src={user.avatar_url}
+            alt=""
+            className="w-10 h-10 rounded-full"
+          />
+          <div className=" flex-col gap-0">
+            <h2 className="text-[14px] font-bold text-zinc-900">{user.name}</h2>
+            <h3 className="text-[10px] text-zinc-800">@{user.login}</h3>
+          </div>
+        </header>
 
-  </figure>
-<section>
-<h2>{"Nome:"}{user.name}</h2>
-<ul>
-<li>{"Total de estrela : "}{totalStart}</li>
-<li>{"Total de seguidores: "}{user.followers}</li>
-<li>{"Total de Repositorios Publicos"}{user.public_repos}</li>
-<p>{"Bio : "}{user.bio}</p>
-<button onClick={saveFavorite}>
-  <Star />
-</button>
-</ul>
+        <section className="w-1/2 flex flex-col items-center gap-2">
+          <ul className="flex gap-4">
+            <li className="w-8 flex flex-col items-center justify-center gap-2">
+              <Archive className="text-yellow-icon" size={18} />{" "}
+              <p className="text-purple-dark font-Montserrat text-[14px] font-bold">
+                {totalStart}
+              </p>
+            </li>
 
-</section>
-</section>
-</>
-  )
+            <li className="w-8 flex flex-col items-center justify-center gap-2">
+              <Star className="text-yellow-icon" size={18} />{" "}
+              <p className="text-purple-dark font-Montserrat text-[14px] font-bold">
+                {user.public_repos}
+              </p>
+            </li>
+
+            <li className="w-8 flex flex-col items-center justify-center gap-2">
+              <User2 className="text-yellow-icon" size={18} />{" "}
+              <p className="text-purple-dark font-Montserrat text-[14px] font-bold">
+                {user.followers}
+              </p>
+            </li>
+          </ul>
+          <button
+            className="w-full h-10 bg-purple-dark text-white font-semibold px-4 rounded-md"
+            onClick={() => saveFavorite}
+          >
+            Adicionar
+          </button>
+        </section>
+      </section>
+    </>
+  );
 }
