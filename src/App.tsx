0fa2b9b764d93380cc1f5111fe4acf98b4a07d85
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-20 13:52:24 -0300

    FEAT : add search components

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,12 @@
-import { useState } from "react";
 import "./App.css";
-import { UserFound } from './components/userfound'
-import { formaterForSearch } from "./utils/textformatter";
-
-
+import { Search } from "./components/SearchUser";
+import { UserFound } from "./components/userfound";
 function App() {
-  const [inputTerm, setinputTerm] = useState("");
-  const [githuber, setgithuber] = useState('')
-
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    const term = formaterForSearch(value);
-    setinputTerm(term);
-  };
-
-  const searchGithubers = () => {
-    setgithuber(inputTerm)
-  }
-
   return (
     <>
       <header>Olá Dinho , bem vindo ao seu starHub</header>
-
-      <section>[banner]</section>
-
-      <form action="">
-        <fieldset>
-          <legend>Nova busca</legend>
-          <label htmlFor="Digite o nickname"></label>
-          <input
-            type="text"
-            value={inputTerm}
-            placeholder="Digite o username do Github"
-            onChange={handleSearch}
-          />{" "}
-          <button type="button" onClick={searchGithubers}>
-            Buscar
-          </button>
-        </fieldset>
-      </form>
-
-      {"Use foud : "}
-
-      {
-      githuber && <UserFound username={githuber} />
-     }
-
-
+      <Search />
+      <UserFound />
     </>
   );
 }
