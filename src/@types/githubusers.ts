bba777d01b182748260ac4482a665427e082d7e8
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-20 13:51:50 -0300

    FEAT : add github details type

--- src/@types/githubusers.ts
+++ src/@types/githubusers.ts
@@ -1,32 +1,11 @@
-export type GithubersType = {
-
-  login? : string;
-  name : string ;
-  loading : boolean;
-  avatar_url : string ;
-  followers : number
-  repos_url : string 
+export interface GithubersType {
+  login?: string;
+  name: string;
+  avatar_url: string;
+  followers: number
+  repos_url: string
   totalStart: number
-  public_repos : number;
-  bio :string 
+  public_repos: number;
+  bio: string
 }
 
-export interface FavoriteProps {
-  name : string;
-  bio : string;
-  avatar_url : string
-  public_repos : number;
-  followers : number;
-  totalStart : number
-}
-
-export type globalDataUser = GithubersType[];
-
-export type useProfileProps = {
-  username: string
-}
-
-export interface UserRepoType {
-  name : string;
-  stargazers_count : number
-}
