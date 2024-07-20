f41b3143974164a4d1f0e242cb185a1068b3d29a
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-20 13:54:04 -0300

    FEAT : create user found componente to search results

--- src/components/userfound.tsx
+++ src/components/userfound.tsx
@@ -1,78 +1,74 @@
-import React from "react"
-import { useGitHubUsers } from "../hooks/fetchhooks"
-import { FavoriteProps, useProfileProps } from "../@types/githubusers"
-import useUserGlobalData from "../store.ts/favoriteusers"
-import { Archive, Star, User2 } from "lucide-react";
+import { Archive, Loader, Star, User2 } from "lucide-react"
+import { foundUserStore } from "../store/githubfound"
 
 
-export const UserFound: React.FC<useProfileProps> = ({ username }) => {
-  const { user, isLoading, isError, totalStart } = useGitHubUsers(username)
-  const {upDateData} = useUserGlobalData()
-  if (isLoading) {
-    return <span>Carregando...</span>
-  }
-  if (isError) {
-    console.log(isError)
-    return <span>Falha ao obter usuário</span>
-  }
-  if (!user) {
-    return <span>Não indentificado</span>
-  }
+export const UserFound = () => {
+  const { isloading, erroFound, userFound, deleteUserFound } = foundUserStore()
 
+  if (isloading) return (<div className="animate animate-spin"><Loader /></div>)
+  if (erroFound) return (<div>Não Encontrado</div>)
 
-
-  const saveFavorite = () =>{
-    const {name, bio, avatar_url , public_repos, followers} = user
-const alldata : FavoriteProps = {name, bio, avatar_url , public_repos, followers, totalStart}
-upDateData(alldata)
-    
-  }
   return (
     <>
-      <section className="flex justify-between items-center bg-purple-light w-[90%] ml-auto mr-auto py-8 px-4 rounded-lg font-Montserrat">
-        <header className="flex border flex-col">
-          <img
-            src={user.avatar_url}
-            alt=""
-            className="w-10 h-10 rounded-full"
-          />
-          <div className=" flex-col gap-0">
-            <h2 className="text-[14px] font-bold text-zinc-900">{user.name}</h2>
-            <h3 className="text-[10px] text-zinc-800">@{user.login}</h3>
-          </div>
-        </header>
+      {
+        userFound.map(({ avatar_url, name, login, public_repos, followers, totalStart }, index) => (
+          <section key={index} className="flex justify-between items-center bg-purple-light w-[90%] ml-auto mr-auto py-8 px-4 rounded-lg font-Montserrat">
+            <header className="flex border flex-col">
+              <img
+                src={avatar_url}
+                alt=""
+                className="w-10 h-10 rounded-full"
+              />
+              <div className=" flex-col gap-0">
+                <h2 className="text-[14px] font-bold text-zinc-900">{name}</h2>
+                <h3 className="text-[10px] text-zinc-800">@{login}</h3>
+              </div>
+            </header>
 
-        <section className="w-1/2 flex flex-col items-center gap-2">
-          <ul className="flex gap-4">
-            <li className="w-8 flex flex-col items-center justify-center gap-2">
-              <Archive className="text-yellow-icon" size={18} />{" "}
-              <p className="text-purple-dark font-Montserrat text-[14px] font-bold">
-                {totalStart}
-              </p>
-            </li>
+            <section className="w-1/2 flex flex-col items-center gap-2">
+              <ul className="flex gap-4">
+                <li className="w-8 flex flex-col items-center justify-center gap-2">
+                  <Archive className="text-yellow-icon" size={18} />{" "}
+                  <p className="text-purple-dark font-Montserrat text-[14px] font-bold">
+                    {public_repos}
+                  </p>
+                </li>
 
-            <li className="w-8 flex flex-col items-center justify-center gap-2">
-              <Star className="text-yellow-icon" size={18} />{" "}
-              <p className="text-purple-dark font-Montserrat text-[14px] font-bold">
-                {user.public_repos}
-              </p>
-            </li>
+                <li className="w-8 flex flex-col items-center justify-center gap-2">
+                  <Star className="text-yellow-icon" size={18} />{" "}
+                  <p className="text-purple-dark font-Montserrat text-[14px] font-bold">
+                    {totalStart}
+                  </p>
+                </li>
 
-            <li className="w-8 flex flex-col items-center justify-center gap-2">
-              <User2 className="text-yellow-icon" size={18} />{" "}
-              <p className="text-purple-dark font-Montserrat text-[14px] font-bold">
-                {user.followers}
-              </p>
-            </li>
-          </ul>
-          <button
-            className="w-full h-10 bg-purple-dark text-white font-semibold px-4 rounded-md"
-            onClick={() => saveFavorite}
-          >
-            Adicionar
-          </button>
-        </section>
-      </section>
+                <li className="w-8 flex flex-col items-center justify-center gap-2">
+                  <User2 className="text-yellow-icon" size={18} />{" "}
+                  <p className="text-purple-dark font-Montserrat text-[14px] font-bold">
+                    {followers}
+                  </p>
+                </li>
+              </ul>
+              <div className="flex gap-2 w-full">
+                <button
+                  className=" h-10 text-purple-dark border-2 border-purple-dark font-semibold px-4 rounded-md"
+                  onClick={deleteUserFound}
+                >
+                  Remover
+                </button>
+                <button
+                  className=" h-10 bg-purple-dark text-white font-semibold px-4 rounded-md"
+
+                >
+                  Adicionar
+                </button>
+              </div>
+
+            </section>
+          </section>
+        ))
+      }
     </>
-  );
+  )
+
+
 }
