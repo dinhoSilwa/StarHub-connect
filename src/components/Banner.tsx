5f0f0d8c246ed9bd8a48501141e2646b429b88b4
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-21 19:19:33 -0300

    FEAT: create banner

---
+++ src/components/Banner.tsx
@@ -0,0 +1,13 @@
+import banner from "../assets/banner/banner01.png"
+export const BannerSection = () =>{
+  return(
+    <>
+      <section className="flex justify-around items-center rounded-md px-2">
+
+        <figure>
+          <img src={banner} alt="oi" className="w-full" />
+        </figure>
+      </section>
+    </>
+  )
+}
