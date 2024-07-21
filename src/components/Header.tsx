f412b0bf0ae84a1ffebab13d5ddfa5199c934b60
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-21 19:20:59 -0300

    FEAT: create header componentes

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { BellIcon } from "lucide-react";
+
+export const Header = () =>{
+  return(
+    <>
+      <header className="h-20 flex items-center justify-start gap-2 px-2">
+        <figure className="bg-red-200 h-10 w-10 rounded-full overflow-hidden">
+          <img src="oi" alt="" />
+        </figure>
+        <h2 className="font-bold text-[14px]">Olá, Dinho👋</h2>
+
+        <section className="flex-1  flex justify-end px-4">
+          <figure className="h-10 w-10 bg-zinc-100 grid place-content-center rounded-full">
+            <BellIcon size={20} className="text-purple-800" />
+          </figure>
+        </section>
+      </header>
+    </>
+  )
+}
