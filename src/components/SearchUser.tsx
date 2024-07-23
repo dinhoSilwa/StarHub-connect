9bd1c36030b7d185be4da00a1cd085f4041aadc8
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-23 07:15:21 -0300

    REFACTOR : change formaterForSearch path

--- src/components/SearchUser.tsx
+++ src/components/SearchUser.tsx
@@ -1,30 +1,39 @@
-import { formaterForSearch } from "../utils/textformatter";
+import { formaterForSearch } from "../utils/stringutils";
 import { userNameTerm } from "../store/searchResults";
 import { searcGithubUser } from "../controller/getgithubers";
 import { foundUserStore } from "../store/githubfound";
 
 export const Search = () => {
-  const { term, updateTerm } = userNameTerm()
-  const { updateUserFound, setLoading, setError } = foundUserStore()
+  const { term, updateTerm } = userNameTerm();
+  const { updateUserFound, setLoading, setError } = foundUserStore();
+
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     const username = formaterForSearch(value);
-    updateTerm(username)
+    updateTerm(username);
   };
 
   const searchUserByUserName = () => {
-    setLoading(true)
+    setLoading(true);
     searcGithubUser(`https://api.github.com/users/${term}`)
-      .then(res => { updateUserFound(res), setLoading(false) })
-      .catch(err => { console.log("Falha ao obter Dados", err), setError(true) }).finally(() => setLoading(false))
-  }
+      .then((res) => {
+        updateUserFound(res), setLoading(false);
+      })
+      .catch((err) => {
+        console.log("Falha ao obter Dados", err), setError(true);
+      })
+      .finally(() => setLoading(false));
+  };
 
   return (
     <>
       <form action="" className="flex flex-col px-2 py-4 gap-2">
-        <label htmlFor="githubsearch"
+        <label
+          htmlFor="githubsearch"
           className="text-[10px] font-semibold text-zinc-700 font-LexendFont"
-        >Buscar no Github</label>
+        >
+          Buscar no Github
+        </label>
         <fieldset className="flex gap-1">
           <input
             id="githubsearch"
@@ -34,7 +43,8 @@ export const Search = () => {
             onChange={handleSearch}
             className=" flex-1 bg-white pl-2 rounded-md py-2 placeholder:text-[12px] font-bold text-purple-800 text-[14px] h-12"
           />{" "}
-          <button type="button"
+          <button
+            type="button"
             onClick={searchUserByUserName}
             className="bg-purple-700  text-white font-medium px-2 rounded-lg text-[12px]"
           >
@@ -43,5 +53,5 @@ export const Search = () => {
         </fieldset>
       </form>
     </>
-  )
-}
+  );
+};
