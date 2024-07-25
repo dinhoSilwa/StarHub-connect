1484199c1c524a7d9c5ff47111da01b607fc02ff
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-25 07:47:07 -0300

    FEAT : add stacklist render

--- src/components/FavoriteList.tsx
+++ src/components/FavoriteList.tsx
@@ -1,10 +1,9 @@
 import { ArchiveIcon, StarIcon, User2 } from "lucide-react";
 import { FavoriteStorage } from "../store/Favorites";
-import { stacksList } from "../model/allfavorites";
 import { extractFirstAndSecondName } from "../utils/stringutils";
 import { GithubersType } from "../@types/githubusers";
 import { useEffect, useState } from "react";
-import clsx from "clsx";
+import { StackListRender } from "./StackList";
 
 export const FavoriteList = () => {
   const { favoriteDataBase } = FavoriteStorage();
@@ -46,28 +45,8 @@ export const FavoriteList = () => {
         >
           Lista de favoritos
         </h2>
-        <nav
-          role="Stack-List-Navigation"
-          className=" overflow-hidden  grid place-content-center"
-        >
-          <ul className="flex overflow-x-auto items-center gap-2 justify-between h-16">
-            {stacksList.map(({ stackname }, index) => {
-              return (
-                <>
-                  <li
-                    role={`${stackname} item da lista`}
-                    title={stackname}
-                    key={index}
-                    className={clsx("w-[230px] px-4 py-2 flex flex-row rounded-full font-semibold font-LexendFont hover:bg-purple-dark hover:text-white text-[12px]", {"bg-purple-dark text-white" : currentStackList === stackname})}
-                    onClick={()=>handleStack(stackname)}
-                  >
-                    {stackname}
-                  </li>
-                </>
-              );
-            })}
-          </ul>
-        </nav>
+<StackListRender handleStack={handleStack} currentStackList={currentStackList} menuList={true} />
+
       </header>
 
       <section className="flex flex-col gap-4">
