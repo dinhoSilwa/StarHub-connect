912a527c7eab633f7dd3f59e94c5a58e5a66e6c0
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-27 13:56:57 -0300

    FIX : notlist height

--- src/components/Notlistyet.tsx
+++ src/components/Notlistyet.tsx
@@ -2,7 +2,7 @@ import notList from '../assets/banner/notyet.png'
 export const NotListYet = ({stack} : {stack : string}) =>{
     return(
         <>
-        <section className='flex flex-col items-center h-64 justify-center gap-4 mt-8'>
+        <section className='flex flex-col items-center justify-start gap-4 mt-8 h-screen'>
             <h3 className='w-[50%] text-center font-LexendFont text-zinc-500 text-[16px] leading-6'>
             Você ainda não adicionou nada <strong>aos seu favoritos {stack === "Todas" ? '' : stack}.</strong>
             </h3>
