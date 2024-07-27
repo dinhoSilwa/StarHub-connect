0cae8b5d8c9a79a85f4f1774d7c2c0d8bba3af15
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-27 14:46:44 -0300

    FIX : ajust components

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,7 +3,7 @@ import bannerIcons from "../assets/banner/banner_01.png"
 export const BannerSection = () => {
   return (
     <>
-      <section className="flex justify-center items-center rounded-xl banner-gradiente h-[132px] w-[90%] ml-auto mr-auto px-6">
+      <section className="flex justify-center items-center rounded-xl banner-gradiente h-[132px] w-[90%] ml-auto mr-auto px-6 mb-4">
         <header className="flex flex-col font-LexendFont  w-[150px] mr-6">
         <h3 className="font-light text-[12px] text-banner-title">Conecte-se aos seus</h3> 
         <h2 className="font-bold leading-[16px] text-banner-title"> Desenvolvedores Favoritos</h2>
