54c097af028bb43060466fa445313806e6d2b732
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-27 14:46:11 -0300

    FEAT : login ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
+import { useState } from "react";
 import "./App.css";
-import { BannerSection } from "./components/Banner";
-import { FavoriteList } from "./components/FavoriteList";
-import { Header } from "./components/Header";
-import { Search } from "./components/SearchUser";
-import { UserFound } from "./components/userfound";
+import { HomePage } from "./components/Home";
 
 function App() {
 
+
+  const [login, setlogin] = useState(false)
+
   return (
     <>
-      <main className="bg-[#f3f1f1]">
-        <Header />
-        <BannerSection />
-        <Search />
-        <UserFound />
-        <FavoriteList />
-      </main>
+    
+{
+  !login ? <main>
+  <div className="flex flex-col items-center">
+    <input type="text" placeholder="digite o seu email" className="border" />
+<input type="password" placeholder="Adicionar Senha" className="border" />
+<button type="button" className="bg-green-500 px-6 py-2 w-[50%]"
+onClick={()=>setlogin(!login)}
+>
+  Login
+</button>
+  </div>
+</main> : <HomePage />
+}
     </>
   );
 }
