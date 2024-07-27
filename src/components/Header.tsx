4b288d506baa8a6ea01bde80707c1d8d9a772393
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-27 14:47:53 -0300

    FIX : header components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,11 @@ import { BellIcon } from "lucide-react";
 export const Header = () => {
   return (
     <>
-      <header className="h-20 flex items-center justify-start gap-2 px-2">
+      <header className="h-20 flex items-center justify-start gap-2 px-4 mb-4">
         <figure className="bg-zinc-200 h-10 w-10 rounded-full overflow-hidden">
-          <img src="oi" alt="" />
+          <img src="profile" alt="" />
         </figure>
-        <h2 className="font-bold text-[14px]">Olá, Dinho👋</h2>
+        <h2 className="font-bold text-[14px] ">Olá, Dinho👋</h2>
 
         <section className="flex-1  flex justify-end px-4">
           <figure className="h-10 w-10 bg-zinc-100 grid place-content-center rounded-full">
@@ -16,5 +16,5 @@ export const Header = () => {
         </section>
       </header>
     </>
-  )
-}
+  );
+};
