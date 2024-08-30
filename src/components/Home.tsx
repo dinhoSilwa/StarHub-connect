6c68c320e692eba0ee68005ca35ce469f7fb9a99
claudio silva <claudiosilva.one@gmail.com>
2024-08-30 08:40:16 -0300

    feat : add profile modal

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,12 +3,14 @@ import { Header } from "./Header";
 import { SearchUser } from "../components/Search/SearchUser";
 import { FavoriteList } from "./favoritelist";
 import { ModalName } from "./Modal/modalname";
+import { ProfileModal } from "./Profile/profile";
 
 export const HomePage = () => {
   return (
     <>
       <main className="flex flex-col pt-4">
         <ModalName />
+        <ProfileModal />
         <Header />
         <BannerSection />
         <SearchUser />
