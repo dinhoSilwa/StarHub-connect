ef8fe836154b2bbe2c111039f598b7480bab9390
claudio silva <claudiosilva.one@gmail.com>
2024-10-25 19:28:36 -0300

    refact : ui ul list

--- src/components/Navegations/EditMenu.tsx
+++ src/components/Navegations/EditMenu.tsx
@@ -5,7 +5,7 @@ export const NavigationStack = () => {
   const [currentStack, setcurrentStack] = useState<string | null>("Todas");
   return (
     <nav role="Stack-List-Navigation w-full" className="overflow-scroll flex">
-      <ul className="flex overflow-x-auto space-x-2">
+      <ul className="flex overflow-x-auto space-x-2 w-[90%] ml-auto mr-auto">
         {stacksList.map(({ icon, stackname }, index) => (
           <li
             className={` ${index == 0 ? "ml-4" : ""} w-28 h-28 font-LexendFont px-12 flex flex-col justify-center items-center gap-2 rounded-md ${currentStack === stackname ? "bg-[#ffc171] font-bold text-bgDark-primary" : "bg-zinc-200 dark:bg-bgDark-second"}`}
