1a890b624b8cd93a483d3e139e634d4689cf3804
claudio silva <claudiosilva.one@gmail.com>
2024-10-29 07:34:57 -0300

    refactor: update the get function to return an array

--- src/service/methods/get.ts
+++ src/service/methods/get.ts
@@ -4,17 +4,19 @@ import type { GithubUserResponse } from "../../@types/githubusers";
 export const getGithubUser = async (
   api: AxiosInstance,
   username: string
-): Promise<GithubUserResponse> => {
+): Promise<GithubUserResponse[]> => {
   const response = await api.get(`/users/${username}`);
   const data = response.data;
 
-  return {
-    login: data.login,
-    name: data.name,
-    avatar_url: data.avatar_url,
-    followers: data.followers,
-    repos_url: data.repos_url,
-    public_repos: data.public_repos,
-    bio: data.bio,
-  };
+  return [
+    {
+      login: data.login,
+      name: data.name,
+      avatar_url: data.avatar_url,
+      followers: data.followers,
+      repos_url: data.repos_url,
+      public_repos: data.public_repos,
+      bio: data.bio
+    }
+  ];
 };
